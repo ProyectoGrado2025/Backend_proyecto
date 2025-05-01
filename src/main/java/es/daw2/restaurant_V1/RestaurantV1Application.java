b1b2dbf883aa933fa7677b38ba9d04705b6163ce
Andrew Salcedo <proyectogradodaw@gmail.com>
2025-05-01 15:52:58 +0200

    chore: initial commit

---
+++ src/main/java/es/daw2/restaurant_V1/RestaurantV1Application.java
@@ -0,0 +1,13 @@
+package es.daw2.restaurant_V1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/daw2/restaurant_V1/RestaurantV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daw2.restaurant_V1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
